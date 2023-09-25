ba0d012a23c032b4711476ff737f0c9777111e95
kisia0916 <fumiharuabe@gmail.com>
2023-09-25 23:11:14 +0900

    change

--- app.ts
+++ app.ts
@@ -1,20 +1,20 @@
 import express from "express"
 import http from "http"
-import {Client} from "@line/bot-sdk"
+const {Client} = require("messaging-api-line")
 import {TextMessage} from "@line/bot-sdk"
 const app = express()
 const server = http.createServer(app)
 
 
 interface TokenType{
-    channelSecret:String;
-    channelAccessToken:String
+    channelSecret:string;
+    channelAccessToken:string
 }
-const config = {
+const config:TokenType = {
     channelSecret:"Ac1Cxy/XBo5PnTRimyGfOXKoRRys5T4EouYGi2Edrsxt/5uFpZy5rYphNmu9TJ0UyjtPDSgV3Z5f8x01HSs+pGrCKkTsiC99J8p50H9HRa9RppDHinzcqhhQIuTVUUSc6H1DQWicvryy29hL0zyzmgdB04t89/1O/w1cDnyilFU=",
     channelAccessToken:"a0a95804e41131afa6b7342053060f5e"
 }
-const client = new Client(config)
+const client = new Client.connrct(config)
 const sendMess = async()=>{
     const messages:TextMessage = {
         type: 'text',
@@ -36,9 +36,10 @@ app.use(
 
 
 app.get("/",async(req,res)=>{
-    sendMess()
+
 })
 
 server.listen(3000,()=>{
+    sendMess()
     console.log("server run")
 })
